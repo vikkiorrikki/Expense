a68c235c2365f8aa065bff5723f0b94ed358b06e
Victoria Saklakova <vikulka58@mail.ru>
2020-03-25 01:06:47 +0300

    add error message if textfields are empty

--- Expense/Controller/NewExpenseViewController.swift
+++ Expense/Controller/NewExpenseViewController.swift
@@ -12,18 +12,47 @@ class NewExpenseViewController: UIViewController {
 
     @IBOutlet weak var expenseNameTextField: UITextField!
     @IBOutlet weak var expenseAmountTextField: UITextField!
+    @IBOutlet weak var errorMessageLabel: UILabel!
     
     weak var delegate: ExpensesViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        errorMessageLabel.isHidden = true
     }
     
     @IBAction func addNewExpenseTouched(_ sender: UIButton) {
-        if let name = expenseNameTextField.text, let amountText = expenseAmountTextField.text, let amount = Double(amountText) {
+        if !((expenseNameTextField.text)!.isEmpty) || !((expenseAmountTextField.text)!.isEmpty) {
+            guard let name = expenseNameTextField.text, !name.isEmpty else {
+                errorMessageLabel.isHidden = false
+                errorMessageLabel.text! = "Name is empty. Please fill field."
+                
+                expenseNameTextField.layer.borderColor = #colorLiteral(red: 0.6643349528, green: 0, blue: 0.08385483176, alpha: 1)
+                expenseNameTextField.layer.borderWidth = 1.0
+                expenseAmountTextField.layer.borderWidth = 0.0
+                return
+            }
+            guard let amountText = expenseAmountTextField.text, let amount = Double(amountText) else {
+                errorMessageLabel.isHidden = false
+                errorMessageLabel.text! = "Amount is empty. Please fill field."
+                
+                expenseAmountTextField.layer.borderColor = #colorLiteral(red: 0.6643349528, green: 0, blue: 0.08385483176, alpha: 1)
+                expenseAmountTextField.layer.borderWidth = 1.0
+                expenseNameTextField.layer.borderWidth = 0.0
+                return
+            }
             let newExpense = Expense(name: name, amount: amount)
             delegate?.addNewExpenseTouched(newExpense: newExpense)
+            self.dismiss(animated: true, completion: nil)
+        } else {
+            errorMessageLabel.isHidden = false
+            errorMessageLabel.text! = "Fields are empty. Please fill fields."
+            
+            expenseNameTextField.layer.borderColor = #colorLiteral(red: 0.6643349528, green: 0, blue: 0.08385483176, alpha: 1)
+            expenseNameTextField.layer.borderWidth = 1.0
+            
+            expenseAmountTextField.layer.borderColor = #colorLiteral(red: 0.6643349528, green: 0, blue: 0.08385483176, alpha: 1)
+            expenseAmountTextField.layer.borderWidth = 1.0
         }
-        self.dismiss(animated: true, completion: nil)
     }
 }
