6ab2853ac0bfeb977174700a6187de2f443e71b7
Victoria Saklakova <vikulka58@mail.ru>
2020-03-28 01:17:59 +0300

    add empty view

--- Expense/Controller/ExpensesViewController.swift
+++ Expense/Controller/ExpensesViewController.swift
@@ -33,6 +33,7 @@ class ExpensesViewController: UIViewController, ExpensesViewControllerDelegate {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.register(UINib(nibName: "ExpenseCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
+        tableView.restore()
 
         expenses = storageService.loadExpenses()
     }
@@ -75,6 +76,11 @@ class ExpensesViewController: UIViewController, ExpensesViewControllerDelegate {
 
 extension ExpensesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if expenses.count == 0{
+            tableView.setEmptyView(title: "You don't have any expenses.", message: "Your expenses will be in here.")
+        } else {
+            tableView.restore()
+        }
         return expenses.count
     }
     
@@ -94,6 +100,49 @@ extension ExpensesViewController: UITableViewDataSource {
         
         storageService.saveExpenses(expenses)
       }
+    }
+}
+
+//MARK: - UITableView
+
+extension UITableView { //can we move it to separate file?
+    func setEmptyView(title: String, message: String) {
+        let emptyView = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height))
+        
+        let titleLabel = UILabel()
+        let messageLabel = UILabel()
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false //??
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.textColor = UIColor.black
+        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
+        messageLabel.textColor = UIColor.lightGray
+        messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+        
+        emptyView.addSubview(titleLabel)
+        emptyView.addSubview(messageLabel)
+        
+        //what is anchor
+        titleLabel.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 20).isActive = true
+        messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -20).isActive = true
+        
+        titleLabel.text = title
+        messageLabel.text = message
+        messageLabel.numberOfLines = 0 //??
+        messageLabel.textAlignment = .center
+        
+        // The only tricky part is here:
+        self.backgroundView = emptyView
+        self.separatorStyle = .none //??
+    }
+    
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine //??
     }
 }
 
